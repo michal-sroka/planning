17185b2d7fc5958f3039a09bb0ec5884f99f2272
Ubuntu <michal@research-ubuntu.research-ubuntu.f6.internal.cloudapp.net>
2017-05-30 18:19:37 +0000

    adding commands

---
+++ commands.sh
@@ -0,0 +1,4 @@
+~/planners/lpg-td-1.0 -o task_scheduling_michal/domain_easy.pddl -f task_scheduling_michal/pfile02_easy.pddl -n 3 -noout
+~/planners/lpg1.2 -o task_scheduling_michal/domain.txt -f task_scheduling_michal/pfile04.pddl -n 5 -noout
+~/planners/lpg-td-old -o task_scheduling_michal/domain.txt -f task_scheduling_michal/pfile02.txt -n 3 -noout
+ ~/planners/metricff -o task_scheduling_michal/domain.txt -f task_scheduling_michal/pfile01.txt -s 4 -w 5
